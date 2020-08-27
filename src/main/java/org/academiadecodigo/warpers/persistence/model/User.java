2299244545a8879859b34b1eeb204f15f89fefcb
Code Cadet <codecadet@perl.local>
2020-08-27 23:32:29 +0100

    subs api test

--- src/main/java/org/academiadecodigo/warpers/persistence/model/User.java
+++ src/main/java/org/academiadecodigo/warpers/persistence/model/User.java
@@ -71,16 +71,16 @@ public class User extends AbstractModel {
         return subscriptions;
     }
 
-    public void addAccount(Subscription subscription) {
+    /*public void addAccount(Subscription subscription) {
         subscriptions.add(subscription);
         subscription.setUser(this);
-    }
+    }*/
 
 
-    public void removeAccount(Subscription subscription) {
+    /*public void removeAccount(Subscription subscription) {
         subscriptions.remove(subscription);
         subscription.setUser(null);
-    }
+    }*/
 
     public String getPassword() {
         return password;
