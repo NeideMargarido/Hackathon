13afbbfb4d48e20f01a7e187491fffa1e717f267
Code Cadet <codecadet@perl.local>
2020-08-27 13:47:25 +0100

    change paths

--- subscriptionManagement/src/main/java/org/academiadecodigo/warpers/command/UserDto.java
+++ subscriptionManagement/src/main/java/org/academiadecodigo/warpers/command/UserDto.java
@@ -39,92 +39,42 @@ public class UserDto {
     @Size(min = 3)
     private String country;
 
-    /**
-     * Gets the id of the customer DTO
-     *
-     * @return the customer DTO id
-     */
     public Integer getId() {
         return id;
     }
 
-    /**
-     * Sets the id of the customer DTO
-     *
-     * @param id the id to set
-     */
     public void setId(Integer id) {
         this.id = id;
     }
 
-    /**
-     * Gets the first name of the customer DTO
-     *
-     * @return the customer DTO first name
-     */
     public String getFirstName() {
         return firstName;
     }
 
-    /**
-     * Sets the first name of the customer DTO
-     *
-     * @param firstName the first name to set
-     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    /**
-     * Gets the last name of the customer DTO
-     *
-     * @return the customer DTO last name
-     */
     public String getLastName() {
         return lastName;
     }
 
-    /**
-     * Sets the last name of the customer DTO
-     *
-     * @param lastName the last name to set
-     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    /**
-     * Gets the email of the customer DTO
-     *
-     * @return the customer DTO email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * Sets the email of the customer DTO
-     *
-     * @param email the email to set
-     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * Gets the phone of the customer DTO
-     *
-     * @return the customer DTO phone
-     */
     public String getPhone() {
         return phone;
     }
 
-    /**
-     * Sets the phone of the customer DTP
-     *
-     * @param phone the phone to set
-     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
@@ -145,17 +95,16 @@ public class UserDto {
         this.country = country;
     }
 
-    /**
-     * @see Object#toString()
-     */
     @Override
     public String toString() {
-        return "CustomerForm{" +
+        return "UserForm{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", phone='" + phone + '\'' +
+                ", password='" + password + '\''+
+                ", county='" + country + '\'' +
                 '}';
     }
 }
