8e5cd7788b6518222cb588e88288dfc279f29a66
Code Cadet <codecadet@perl.local>
2020-08-28 02:39:59 +0100

    subs api test

--- src/main/java/org/academiadecodigo/warpers/converters/SubscriptionDtoToSubscription.java
+++ src/main/java/org/academiadecodigo/warpers/converters/SubscriptionDtoToSubscription.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.Flow;
 
 
 @Component
